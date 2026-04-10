ddcbea0f133ad0702d5b112948a2277b48a736e7
Eslam Mohamed <eslam.mohamed202220@gmail.com>
2026-04-11 00:10:57 +0200

    h

--- Core/Game.cpp
+++ Core/Game.cpp
@@ -144,7 +144,7 @@ void Game::updateTimer() {
 }
 void Game::Wolfadd() {
 
-	if (timer % 12 == 0&& wolf_Show) {
+	if (timer % 11 == 0&& !wolf_Show) {
 		point p;
 		std::random_device rd1;
 		std::mt19937 gen1(rd1());
@@ -154,12 +154,12 @@ void Game::Wolfadd() {
 		std::mt19937 gen2(rd2());
 		std::uniform_int_distribution<int> dist2(2 * config.toolBarHeight, config.windHeight - config.statusBarHeight - 100);
 		p.y = dist2(gen2);
-		animalsList[animalCount] = new Wolf(this, p, 70, 70, "images\\wolf.webp");
+		animalsList[animalCount] = new Wolf(this, p, 70, 70, "images\\wolf.jpg");
 		animalsList[animalCount]->draw();
 		animalCount++;
 		wolf_Show = true;
 	}
-	if (timer % 12 !=0) {
+	if (timer % 11 !=0) {
 		wolf_Show = false;
 	}
 	
